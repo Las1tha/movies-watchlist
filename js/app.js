dbe15dafe9510b74297673c760ed4a6874b2b5a9
Las1tha <lasithakelum55@gmail.com>
2025-08-21 10:37:21 +0530

    app

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
   const yearInput = document.getElementById('year');
   const directorInput = document.getElementById('director');
   const genreInput = document.getElementById('genre');
-  const statusRadios = document.getElementByName('status');
+  const statusRadios = document.getElementsByName('status');
   const ratingInput = document.getElementById('rating');
   const notesInput = document.getElementById('notes');
   const saveBtn = document.getElementById('saveBtn');
@@ -33,5 +33,57 @@ const firebaseConfig = {
   const watchlistCount = document.getElementById('watchlistCount');
   const watchedCount = document.getElementById('watchedCount');
   
+  const stars= document.querySelectorAll('.rating-stars i');
+
+  //initialize the app
+  function init(){
+    stars.forEach(star =>{
+           star.addEventListener('click',handleStarClick);
+           star.addEventListener('mouseover',handleStarHover);
+           star.addEventListener('mouseout',handleStarOut);
+    });
+  }
   
-  
+  //star rating functionalities
+  function handleStarClick(e){
+    const rating = parseInt(e.target.dataset.rating);
+    ratingInput.value = rating;
+    updateStarRating(rating);
+  }
+
+  function handleStarHover(e){
+      const rating = parseInt(e.target.dataset.rating);
+      hilightStars(rating);
+  }
+  function handleStarOut(){
+    const currentRating = parseInt(ratingInput.value);
+    hilightStars(currentRating);
+  }
+
+  function hilightStars(cont){
+    stars.forEach((star,index)=>{
+      if(index<cont){
+        star.classList.add('active');
+        
+      }else{
+        star.classList.remove('active');
+        
+      }
+    });
+
+  }
+  function updateStarRating(rating){
+    stars.forEach((star,index)=>{
+      if(index<rating){
+        star.classList.add('active');
+        star.classList.remove('far');
+        star.classList.add('fas');
+      }else{
+        star.classList.remove('active');
+        star.classList.remove('fas');
+        star.classList.add('far');
+      }
+    }); 
+  }
+
+document.addEventListener('DOMContentLoaded', init);
